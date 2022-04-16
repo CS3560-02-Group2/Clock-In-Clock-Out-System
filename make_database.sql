3e52c2e2cd4be44438d46e2e9de1de49e8dab5c8
Alex G <alxgomez123@gmail.com>
2022-04-15 20:30:41 -0700

    barebones database

---
+++ make_database.sql
@@ -0,0 +1,19 @@
+CREATE TABLE employee (
+    empID INT PRIMARY KEY,
+    name VARCHAR(40),
+    emailAddress VARCHAR(40),
+    address VARCHAR(40),
+    phoneNumber INT,
+    position INT
+);
+
+CREATE TABLE timeLog (
+    logID INT PRIMARY KEY,
+    empID INT,
+    date DATE,
+    timeClockedIn DATETIME,
+    timeClockedOut DATETIME,
+    breakStart DATETIME,
+    breakEnd DATETIME,
+    FOREIGN KEY(empID) REFERENCES employee(empID) ON DELETE SET NULL
+);
