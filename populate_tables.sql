bf8e096ce28779c30aa8a92a0c4bbdac1750b01b
Alex G <alxgomez123@gmail.com>
2022-04-17 18:28:07 -0700

    added sql file that will populate tables with data

---
+++ populate_tables.sql
@@ -0,0 +1,6 @@
+-- Since primary key empID is auto incremented, NULL used in insert
+INSERT INTO employee VALUES
+    (NULL, "John Doe", "jdoe@cpp.edu", "9483 Bingus Ave.", "1234567890", 0),
+    (NULL, "Some Guy", "sGuy@cpp.edu", "3294 Nowhere St.", "2948274958", 0),
+    (NULL, "Lena Raine", "lRaine@cpp.edu", "3849 Big Blvd.", "1058269803", 0),
+    (NULL, "Joe Zeke", "jZeke@cpp.edu", "5958 Golden Way", "3049584730", 1);
