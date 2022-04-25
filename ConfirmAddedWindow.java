10d2ec8ca7e20423b2cc8ce8fb04d640d60bf9ac
Taylor Vandenberg <36976156+TaylorVandenberg@users.noreply.github.com>
2022-04-25 13:11:41 -0700

    Update ConfirmAddedWindow.java

--- ConfirmAddedWindow.java
+++ ConfirmAddedWindow.java
@@ -29,7 +29,7 @@ public class ConfirmAddedWindow extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.HIDE_ON_CLOSE);
 
         jLabel1.setText("Employee added.");
 
