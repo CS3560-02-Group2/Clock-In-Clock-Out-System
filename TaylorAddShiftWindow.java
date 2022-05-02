6a844f6fdd0cb22ee76b6957816bebcc1cb73a42
Taylor Vandenberg <36976156+TaylorVandenberg@users.noreply.github.com>
2022-05-02 13:33:48 -0700

    Update TaylorAddShiftWindow.java

--- TaylorAddShiftWindow.java
+++ TaylorAddShiftWindow.java
@@ -1,6 +1,3 @@
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-
 import javax.swing.JOptionPane;
 
 /*
@@ -41,6 +38,8 @@ public class TaylorAddShiftWindow extends javax.swing.JFrame {
         clockOutTextField = new javax.swing.JTextField();
         clockInTextField = new javax.swing.JTextField();
         idTextField = new javax.swing.JTextField();
+        jLabel6 = new javax.swing.JLabel();
+        dateTextField = new javax.swing.JTextField();
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
 
@@ -62,6 +61,8 @@ public class TaylorAddShiftWindow extends javax.swing.JFrame {
             }
         });
 
+        jLabel6.setText("Date");
+
         jButton1.setText("Cancel");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -82,16 +83,8 @@ public class TaylorAddShiftWindow extends javax.swing.JFrame {
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(jButton2)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jButton1))
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                        .addGroup(jPanel1Layout.createSequentialGroup()
-                            .addComponent(jLabel5)
-                            .addGap(18, 18, 18)
-                            .addComponent(breakEndTextField))
                         .addGroup(jPanel1Layout.createSequentialGroup()
                             .addComponent(jLabel4)
                             .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
@@ -105,7 +98,19 @@ public class TaylorAddShiftWindow extends javax.swing.JFrame {
                             .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                 .addComponent(clockOutTextField)
                                 .addComponent(clockInTextField)
-                                .addComponent(idTextField)))))
+                                .addComponent(idTextField)))
+                        .addGroup(jPanel1Layout.createSequentialGroup()
+                            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                .addComponent(jLabel5)
+                                .addComponent(jLabel6))
+                            .addGap(18, 18, 18)
+                            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                .addComponent(breakEndTextField)
+                                .addComponent(dateTextField))))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addComponent(jButton2)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jButton1)))
                 .addContainerGap())
         );
         jPanel1Layout.setVerticalGroup(
@@ -131,11 +136,15 @@ public class TaylorAddShiftWindow extends javax.swing.JFrame {
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel5)
                     .addComponent(breakEndTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 12, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(dateTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel6))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jButton1)
                     .addComponent(jButton2))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap())
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -166,23 +175,12 @@ public class TaylorAddShiftWindow extends javax.swing.JFrame {
         String breakStart = breakStartTextField.getText();
         String breakEnd = breakEndTextField.getText();
         int id = Integer.parseInt(idTextField.getText());
-
-        Calendar calendar = Calendar.getInstance();
-        SimpleDateFormat dataBaseFormatter = new SimpleDateFormat("yyyy-MM-dd");
-        String currentDateDataBase = dataBaseFormatter.format(calendar.getTime());
+        String date = dateTextField.getText();
         
-        if (!beginShift.equals("")) {
-            SendToDatabase.addBeginShift(id, currentDateDataBase, beginShift);
-            if (!endShift.equals("")) {
-                SendToDatabase.addEndShift(id, currentDateDataBase, endShift);
-            }
-            if (!breakStart.equals("")) {
-                SendToDatabase.addBeginBreak(id, currentDateDataBase, breakStart);
-            }
-            if (!breakEnd.equals("")) {
-                SendToDatabase.addEndBreak(id, currentDateDataBase, breakEnd);
-            }
-        }
+        SendToDatabase.addBeginShift(id, date, beginShift);
+        SendToDatabase.addEndShift(id, date, endShift);
+        SendToDatabase.addBeginBreak(id, date, breakStart);
+        SendToDatabase.addEndBreak(id, date, breakEnd);
         
         JOptionPane.showMessageDialog(null,
                 "Shift Added",
@@ -230,6 +228,7 @@ public class TaylorAddShiftWindow extends javax.swing.JFrame {
     private javax.swing.JTextField breakStartTextField;
     private javax.swing.JTextField clockInTextField;
     private javax.swing.JTextField clockOutTextField;
+    private javax.swing.JTextField dateTextField;
     private javax.swing.JTextField idTextField;
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
@@ -238,6 +237,7 @@ public class TaylorAddShiftWindow extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
     private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
